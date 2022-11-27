194a823e916c2c5edef4bb3afaeca01d793f03ea
L. Scott Blankenship <pcxtsbl@scottUoN.lan>
2022-11-27 15:28:44 +0000

    no vim dir

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc bash_aliases bash_history vim vimrc"    # list of files/folders to symlink in homedir
+files="bashrc bash_aliases bash_history vimrc"    # list of files/folders to symlink in homedir
 
 ##########
 
